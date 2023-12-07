20c47661d1cbbfa93516f02bd292086f524e1524
leoloso <leo@getpop.org>
2023-12-07 09:14:36 +0000

    prepare release

--- gatographql-testing-schema.php
+++ gatographql-testing-schema.php
@@ -3,7 +3,7 @@
 Plugin Name: Gato GraphQL - Testing Schema
 Plugin URI: https://github.com/GatoGraphQL/GatoGraphQL
 Description: Addition of elements to the GraphQL schema to test the Gato GraphQL plugin
-Version: 1.2.0
+Version: 1.3.0
 Requires at least: 5.4
 Requires PHP: 7.2
 Author: Gato GraphQL
@@ -52,7 +52,7 @@ add_action(
          *
          * @gatographql-readonly-code
          */
-        $extensionVersion = '1.2.0';
+        $extensionVersion = '1.3.0';
         $extensionName = \__('Gato GraphQL - Testing Schema', 'gatographql-testing-schema');
         /**
          * Important: Do not modify the formatting of this PHP code!
@@ -63,7 +63,7 @@ add_action(
          *
          * @gatographql-readonly-code
          */
-        $mainPluginVersionConstraint = '^1.2';
+        $mainPluginVersionConstraint = '^1.3';
         
         /**
          * Validate Gato GraphQL is active
@@ -108,7 +108,7 @@ add_action(
          *
          * @gatographql-readonly-code
          */
-        $commitHash = '61cc94a77a8dd4ca3369da484ad8cf001cec8d30';
+        $commitHash = 'ad5dbe462ff65f2ed40e334e54178bc24836cd6b';
 
         // Load Composer’s autoloader
         require_once(__DIR__ . '/vendor/autoload.php');
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit091f1b0232982b7a182e540f6228727e::getLoader();
+return ComposerAutoloaderInit5d8c3e6d14724d1fcff35ef829377b47::getLoader();
