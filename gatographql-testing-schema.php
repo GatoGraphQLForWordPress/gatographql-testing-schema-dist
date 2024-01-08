fb16543c90205ec7e2ce3a423d0ee5ed3a855aa2
leoloso <leo@getpop.org>
2024-01-08 06:14:23 +0000

    Merge d16e104f6af0e20826e640a3816943ae485881a3 into f8579139f1af05aae171e776c6f53b0c458e339a

--- gatographql-testing-schema.php
+++ gatographql-testing-schema.php
@@ -108,7 +108,7 @@ add_action(
          *
          * @gatographql-readonly-code
          */
-        $commitHash = 'fccc36996036700ffe3981e2b3d32bf412f2afb3';
+        $commitHash = 'addd21e3b8a4c5e5c8268dac940e0d141a9876bd';
 
         // Load Composer’s autoloader
         require_once(__DIR__ . '/vendor/autoload.php');
